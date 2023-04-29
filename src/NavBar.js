88f9c149c44edc3b439bee6d8c8773a6a4641401
jphill4884 <justinkphillips@gmail.com>
2023-04-29 11:18:44 -0700

    change the structure of the main page

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,15 +5,9 @@ import "./NavBar.css";
 export default function NavBar() {
   return (
     <div className="navbar">
-      <div className="logo">
-        <img src={require("./img/logo.jpg")} alt="developer's logo with initials and full name" className="logo-image"/>
-      </div>
-      <div className="middle"></div>
-      <div className="right">
         <span className="navbar-link">Projects</span>
         <span className="navbar-link">Something</span>
         <span className="navbar-link">Resume</span>
       </div>
-    </div>
   );
 }
