3128469de008a59d8977aeb5ee1870c14fa13b77
jphill4884 <justinkphillips@gmail.com>
2023-05-04 16:29:52 -0700

    add loader spinner

--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import PropagateLoader from 'react-spinners/PropagateLoader';
-import Client from './Contentful';
-import ProjectImage from './ProjectPageImage';
-import NavBar from './NavBar';
-import './NavBar.css';
-import Footer from './Footer';
-import './Footer.css';
-import './ProjectPage.css';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import RingLoader from "react-spinners/RingLoader";
+import Client from "./Contentful";
+import ProjectImage from "./ProjectPageImage";
+import NavBar from "./NavBar";
+import "./NavBar.css";
+import Footer from "./Footer";
+import "./Footer.css";
+import "./ProjectPage.css";
 
 function ProjectPage() {
   const { projectId } = useParams();
 
-  const [ isLoading, setIsLoading ] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const  [webProject, setWebProject ] = useState();
+  const [webProject, setWebProject] = useState();
 
   useEffect(() => {
     Client.getEntry(projectId).then((webProjectData) =>
@@ -24,27 +24,41 @@ function ProjectPage() {
   }, [projectId, setIsLoading]);
 
   if (isLoading) {
-    return <PropagateLoader color="#C0C0C0" size={350} speed={20} />;
-  } else if (!webProject){
-    return <PropagateLoader color="#C0C0C0" size={350} speed={20} />;
+    return (
+      <div className="loader">
+        <RingLoader color="#C0C0C0" size={300} speed={20} />
+      </div>
+    );
+  } else if (!webProject) {
+    return (
+      <div className="loader">
+        <RingLoader color="#C0C0C0" size={350} speed={20} />
+      </div>
+    );
   } else {
     return (
       <div>
-      <NavBar />
-      <div className="main">
-        <h1>{webProject && webProject.fields.name}</h1>
-        <img
-          src={"https:" + webProject && webProject.fields.coverImage.fields.file.url}
-          className="project-image"
-          alt="screencap of project"
-        />
-        <div className="project-description">
-          <span>{webProject && webProject.fields.description}</span>
-          <hr />
-        </div>
-        <div className="pic-collage">
-          {webProject && webProject.fields.projectImages.map((item, index) => <ProjectImage props={item} key={index} />)}
-        </div>
+        <NavBar />
+        <div className="main">
+          <h1>{webProject && webProject.fields.name}</h1>
+          <img
+            src={
+              "https:" + webProject &&
+              webProject.fields.coverImage.fields.file.url
+            }
+            className="project-image"
+            alt="screencap of project"
+          />
+          <div className="project-description">
+            <span>{webProject && webProject.fields.description}</span>
+            <hr />
+          </div>
+          <div className="pic-collage">
+            {webProject &&
+              webProject.fields.projectImages.map((item, index) => (
+                <ProjectImage props={item} key={index} />
+              ))}
+          </div>
         </div>
         <Footer />
       </div>
