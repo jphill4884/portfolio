95102e7d03302a660c63064c3d430dcfeab71f91
jphill4884 <justinkphillips@gmail.com>
2023-05-05 13:22:28 -0700

    add download button to resume page

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -8,6 +8,14 @@ function Resume() {
   return (
     <div>
       <NavBar />
+      <div className="download-button">
+        <a
+          href="../img/Kyle_Phillips_Resume.pdf"
+          download="Kyle_Phillips_Resume.pdf"
+        >
+          <button type="button">Download</button>
+        </a>
+      </div>
       <div className="resume">
         <SideBar />
         <Main />
